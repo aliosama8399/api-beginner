d11d3a7732626652ea014edf7eedff078a78e67d
unknown <aliosama8399@gmail.com>
2021-09-08 15:47:49 +0200

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts=Post::get();
+        $msg=['OK'];
+        return response($posts,200,$msg);
+    }
+
+
+
+
+
+
+
+}
